0222fcabb02cc134b6e5dde51bdcfbef86bd541e
Nguyen Huy Hoang <137654109+huyhoang8704@users.noreply.github.com>
2024-09-21 00:09:30 +0700

    LeetCode 2. Add Two Numbers

--- text.cpp
+++ text.cpp
@@ -2,17 +2,51 @@
 using namespace std;
 typedef long long ll;
 
+//! LeetCode 2. Add Two Numbers
+//! https://leetcode.com/problems/add-two-numbers
 
+ struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-        map<int,int> mp;
-        for(auto x : nums) mp[x]++;
-        for(auto x : mp) {
-            if(x.second > nums.size()/2) return x.first;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* tail = dummyHead;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int digit1 = (l1 != nullptr) ? l1->val : 0;
+            int digit2 = (l2 != nullptr) ? l2->val : 0;
+
+            int sum = digit1 + digit2 + carry;
+            int digit = sum % 10; // Phần dư
+            carry = sum / 10; // Phần nguyên
+
+            ListNode* newNode = new ListNode(digit);
+            tail->next = newNode;
+            tail = tail->next;
+
+            l1 = (l1 != nullptr) ? l1->next : nullptr;
+            l2 = (l2 != nullptr) ? l2->next : nullptr;
         }
+
+        ListNode* result = dummyHead->next;
+        delete dummyHead;
+        return result;
     }
 };
+/*
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+*/
+
+
 
 int main(){
     #ifndef ONLINE_JUDGE;
@@ -20,9 +54,9 @@ int main(){
     freopen("output.txt","w",stdout);
     #endif
 
-    vector<int> nums = {2,2,1,1,1,2,2};
-    int val = 3;
-    Solution obj;
-    cout << obj.majorityElement(nums);
+    // vector<int> nums = {2,2,1,1,1,2,2};
+    // int val = 3;
+    // Solution obj;
+    // cout << obj.majorityElement(nums);
     return 0;
 }
