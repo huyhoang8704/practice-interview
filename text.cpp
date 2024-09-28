f21b59fd8738e3cfc032ec9b8cafa0da6dd831e9
Nguyen Huy Hoang <137654109+huyhoang8704@users.noreply.github.com>
2024-09-28 16:48:37 +0700

    LeetCode 205. Isomorphic Strings

--- text.cpp
+++ text.cpp
@@ -6,14 +6,26 @@ typedef long long ll;
 
 class Solution {
 public:
-    bool canConstruct(string ransomNote, string magazine) {
-        map<char,int> mp;
-        for(char x : magazine) mp[x]++;
-        for(char x : ransomNote) {
-            if(mp[x] == 0) return false;
-            mp[x]--;
+    bool isIsomorphic(string s, string t) {
+        vector<int> indexS(200, 0); // Stores index of characters in string s
+        vector<int> indexT(200, 0); // Stores index of characters in string t
+        
+        int len = s.length(); // Get the length of both strings
+        
+        if(len != t.length()) { // If the lengths of the two strings are different, they can't be isomorphic
+            return false;
         }
-        return true;
+        
+        for(int i = 0; i < len; i++) { // Iterate through each character of the strings
+            if(indexS[s[i]] != indexT[t[i]]) { // Check if the index of the current character in string s is different from the index of the corresponding character in string t
+                return false; // If different, strings are not isomorphic
+            }
+            
+            indexS[s[i]] = i + 1; // updating position of current character
+            indexT[t[i]] = i + 1;
+        }
+        
+        return true; // If the loop completes without returning false, strings are isomorphic
     }
 };
 
@@ -24,8 +36,8 @@ int main(){
     #endif
 
     Solution obj;
-    string ransomNote = "aa";
-    string magazine = "aab";
-    cout << obj.canConstruct(ransomNote, magazine);
+    string s = "egg";
+    string t = "add";
+    cout << obj.isIsomorphic(s,t);
     return 0;
 }
